b2effa9fa4fc2b6e9b6e6dc0bfe613d9b47a7d74
isabel maria <isa_antas@hotmail.com>
2018-04-21 13:27:37 +0200

    sobrecargade mas y menos en vector2D

--- SuperMario/src/vector2D.cpp
+++ SuperMario/src/vector2D.cpp
@@ -2,11 +2,30 @@
 
 
 
-vector2D::vector2D()
+vector2D::vector2D(float xv = 0.0f, float yv = 0.0f)
 {
+	x = xv;
+	y = yv;
 }
 
 
 vector2D::~vector2D()
 {
 }
+
+vector2D vector2D::operator-(vector2D &v)
+{
+	vector2D ret;
+	ret.x = x - v.x;
+	ret.y = y - v.y;
+	return ret;
+}
+
+vector2D vector2D::operator+(vector2D & v)
+{
+	vector2D ret;
+	ret.x = x + v.x;
+	ret.y = y + v.y;
+	return ret;
+}
+	
--- SuperMario/src/vector2D.h
+++ SuperMario/src/vector2D.h
@@ -1,9 +1,19 @@
 #pragma once
 class vector2D
 {
-public:
-	vector2D();
-	~vector2D();
+ //atributos
+	float x;
+	float y;
+public: //métodos
+	vector2D(float xv = 0.0f, float yv = 0.0f); // (1)
+	virtual ~vector2D();
+	float modulo(); // (2) modulo del vector
+	float argumento(); // (3) argumento del vector
+	vector2D Unitario(); // (4) devuelve un vector unitario
+	vector2D operator - (vector2D &v); // (5) resta de vectores
+	vector2D operator + (vector2D &v); // (6) suma de vectores
+	float operator *(vector2D &v); // (7) producto escalar
+	vector2D operator *(float); // (8) producto por un escalar 
 
 };
 
